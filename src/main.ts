37038b1ccf7dc059afc21f0e3536ef7b50a9235b
Gal finger <68272808+galfinger212@users.noreply.github.com>
2025-01-25 00:01:24 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,24 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type,Authorization',
   });
 
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+      res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      );
+      res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type,Authorization',
+      );
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
+      res.status(204).send(); // Respond with no content
+    } else {
+      next();
+    }
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
