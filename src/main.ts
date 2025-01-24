009d1317fe2a30e51420e6ff25bda16d47442e72
Gal finger <68272808+galfinger212@users.noreply.github.com>
2025-01-24 23:41:57 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL, // Frontend URL
-    credentials: true, // Allow cookies or Authorization headers
+    origin: '*',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
   });
+
   const port = process.env.PORT || 3000;
-  await app.listen(port); // Ensure the backend runs on a specific port
+  await app.listen(port);
 }
 bootstrap();
