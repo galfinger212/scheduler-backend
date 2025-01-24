db1b715bcb0cf1a2e3360d38d60726ab0f329ac9
Gal finger <68272808+galfinger212@users.noreply.github.com>
2025-01-25 00:16:02 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -9,31 +9,6 @@ async function bootstrap() {
   app.enableCors({
     origin:
       process.env.FRONTEND_URL || 'https://jacuzzi-slots-gimel.netlify.app', // Allow frontend URL
-    credentials: true, // Allow cookies and Authorization headers
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type,Authorization', // Allow these headers
-  });
-
-  app.use((req, res, next) => {
-    console.log('Request Origin:', req.headers.origin);
-    console.log('Request Method:', req.method);
-
-    res.setHeader(
-      'Access-Control-Allow-Origin',
-      process.env.FRONTEND_URL || 'https://jacuzzi-slots-gimel.netlify.app',
-    );
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    );
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-    if (req.method === 'OPTIONS') {
-      return res.status(204).end(); // Respond to preflight request
-    }
-
-    next();
   });
 
   const port = process.env.PORT || 3000;
