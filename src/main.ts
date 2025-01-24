0bd9235f525711a76acc5f14d8142cd3ad2726ce
Gal finger <68272808+galfinger212@users.noreply.github.com>
2025-01-24 23:49:28 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  console.log('process.env.FRONTEND_URL', process.env.FRONTEND_URL);
+
   app.enableCors({
-    origin: '*',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
