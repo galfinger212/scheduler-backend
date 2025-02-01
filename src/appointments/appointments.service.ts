dc646f49ea9a5b87d4ea51d2f686143971169d70
Gal finger <68272808+galfinger212@users.noreply.github.com>
2025-02-01 15:56:48 +0200

    Add files via upload

--- src/appointments/appointments.service.ts
+++ src/appointments/appointments.service.ts
@@ -161,7 +161,7 @@ export class AppointmentsService {
 
     // Send an SMS to the user
     if (appointment.user.phoneNumber) {
-      const message = `שלום ${appointment.user.fullName}, התור שלך לתאריך ${new Date(appointment.date).toLocaleDateString()} בשעה ${appointment.hour}:00 אושר בהצלחה!`;
+      const message = `שלום ${appointment.user.fullName}, התור שלך לגקוזי לתאריך ${new Date(appointment.date).toLocaleDateString()} בשעה ${appointment.hour}:00 אושר בהצלחה!`;
       await this.twilioService.sendSms(appointment.user.phoneNumber, message);
     }
 
